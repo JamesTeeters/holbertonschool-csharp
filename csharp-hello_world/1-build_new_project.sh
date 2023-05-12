a5915e25b95e996cb00a88eaba2685a2d637afb7
JamesTeeters <104854850+JamesTeeters@users.noreply.github.com>
2023-05-12 13:41:59 -0500

    Update 1-build_new_project.sh

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
-# Script that initializes and builds a new C# project inside a folder titled 1-new_project
-
-# Create folder and enter it
-mkdir -p 1-new_project; cd 1-new_project
-dotnet new console
-sleep 3
-dotnet build
-cd ..
+dotnet new console -o 1-new_project && dotnet build 1-new_project
