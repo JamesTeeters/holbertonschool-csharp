bbc115029749aac7d4248661311f0f3401b76219
JamesTeeters <4925@holbertonstudents.com>
2023-05-15 18:38:15 -0500

    replace element

---
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        int[] new_array = array;
+
+        if (index > array.Length -1 || index < 0)
+        {
+            Console.WriteLine("Index out of range");
+            return (array);
+        }
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (index == i) 
+            {
+                new_array[i] = n;
+            }    
+        }
+
+        return (new_array);
+    }
+}
