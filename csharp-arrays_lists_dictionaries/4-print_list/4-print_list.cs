d1d8e2e9f67112de3c0f05c5111069e92e351d67
JamesTeeters <4925@holbertonstudents.com>
2023-05-16 14:58:04 -0500

    create list of int

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> new_list = new List<int>();
+        if (size == 0)
+            Console.WriteLine("");
+        
+        else
+        {
+            for (int i = 0; i < size; i++)
+            {
+                new_list.Add(i);
+                Console.Write(new_list[i]);
+                if (i < size - 1)
+                    Console.Write(" ");
+            }
+            Console.WriteLine("");
+        }
+        return (new_list);
+    }
+}
