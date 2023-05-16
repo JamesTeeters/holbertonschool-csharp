025777908ff68aacb1dd0416d6ea392c06063e3f
JamesTeeters <4925@holbertonstudents.com>
2023-05-16 17:46:56 -0500

    sorted dict

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> newList = new List<string>();
+
+        foreach(KeyValuePair<string, string> pair in myDict)
+        {
+            newList.Add(pair.Key.ToString() + ": " + pair.Value.ToString());
+        }
+
+        newList.Sort();
+
+        foreach (string keyValuePair in newList)
+            Console.WriteLine(keyValuePair);
+    }
+}
