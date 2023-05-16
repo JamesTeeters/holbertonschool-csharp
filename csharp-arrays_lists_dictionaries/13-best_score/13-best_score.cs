bcb197554b2ed6a5e4a2acd93a1a82de24e7207a
JamesTeeters <4925@holbertonstudents.com>
2023-05-16 17:59:31 -0500

    best score

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList == null || myList.Count == 0)
+            return ("None");
+
+        KeyValuePair<string, int> bestScore = myList.First();
+
+        foreach (KeyValuePair<string, int> pair in myList)
+        {
+            if (pair.Value > bestScore.Value)
+                bestScore = pair;
+        }
+        return bestScore.Key;
+    }
+}
