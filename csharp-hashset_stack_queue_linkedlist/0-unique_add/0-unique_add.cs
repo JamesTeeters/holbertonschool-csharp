708ce78a6b86328909b962cbdccfef1dae94a848
JamesTeeters <4925@holbertonstudents.com>
2023-05-17 19:42:31 -0500

    hashset, stack, queue, linked list

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+
+        foreach (int number in myList.Distinct())
+        {
+            sum += number;
+        }
+
+        return (sum);
+    }
+}
