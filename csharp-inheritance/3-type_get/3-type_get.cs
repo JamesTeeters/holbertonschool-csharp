36a8238a7dab08e7cd8f719f4d1d4d93774c71f5
JamesTeeters <4925@holbertonstudents.com>
2023-05-24 15:41:27 -0500

    name oproperties and methods of an Obj

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        string className = myObj.GetType().Name;
+
+        Console.WriteLine("{0} Properties:", className);
+
+        foreach (var item in myObj.GetType().GetProperties())
+        {
+            Console.WriteLine(item.Name);
+        }
+
+        Console.WriteLine("{0} Methods:", className);
+
+        foreach (var item in myObj.GetType().GetMethods())
+        {
+            Console.WriteLine(item.Name);
+        }
+    }
+}
