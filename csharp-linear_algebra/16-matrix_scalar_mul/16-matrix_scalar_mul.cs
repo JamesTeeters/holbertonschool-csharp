feb8a2b830e83bd724ec92a1332cf33ed3fd2bd6
JamesTeeters <4925@holbertonstudents.com>
2023-06-01 17:25:52 -0500

    wrong operator

--- csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -13,7 +13,7 @@ class MatrixMath
 
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
-            for (int j = 0; j > matrix.GetLength(0); j++)
+            for (int j = 0; j < matrix.GetLength(0); j++)
             {
                 matrix[i,j] *= scalar;
             }
