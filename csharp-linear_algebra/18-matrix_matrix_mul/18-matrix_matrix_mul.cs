214ecf0dae6c886fb7fa75599ca30cd45fd3c448
JamesTeeters <4925@holbertonstudents.com>
2023-06-01 18:25:49 -0500

    multiply 2 matrices

---
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collection;
+
+class MatrixMath
+{
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        if (matrix1 is double[,] && matrix2 is double[,] &&
+            matrix2.GetLength(0) == matrix1.GetLength(1))
+        {
+            int mRow1 = matrix1.GetLength(0);
+            int mCol1 = matrix1.GetLength(1);
+            int mRow2 = matrix2.GetLength(0);
+            int mCol2 = matrix2.GetLength(1);
+
+            double[,] matrixMul = new double[mRow1, mCol2];
+
+            for (int col = 0; col < mCol1; col++)
+            {
+                for (int row = 0; row < mRow1; row++)
+                {
+                    for (int i = 0; i < mCol2; i++)
+                    {
+                        matrixMul[row, i] += matrix1[row, col] * matrix2[col, i];
+                    }
+                }
+            } 
+            return matrixMul;
+        }
+        else 
+        {
+            return new double[,]{{-1}};
+        }
+    }
+}
