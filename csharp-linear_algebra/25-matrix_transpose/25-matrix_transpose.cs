141196908d22577774bce0038edae2d80bf427ae
JamesTeeters <4925@holbertonstudents.com>
2023-06-01 19:23:42 -0500

    transpose a matrix

---
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+        if (matrix.Length == 0)
+            return new double[,] {};
+
+        double[,] transposeMatrix = new double[matrix.GetLength(1), matrix.GetLength(0)];
+        
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                transposeMatrix[col, row] = Math.Round(matrix[row, col], 2);
+            }
+        }
+        return transposeMatrix;
+    }
+}
