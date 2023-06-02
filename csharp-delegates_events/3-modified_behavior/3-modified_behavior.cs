5790adf2f3bee731d24d776107682ac3e5526e25
JamesTeeters <4925@holbertonstudents.com>
2023-06-01 23:04:07 -0500

    added moddifiers

---
+++ csharp-delegates_events/3-modified_behavior/3-modified_behavior.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public delegate void  CalculateHealth(float property);
+
+public enum Modifier
+{
+    Weak,
+    Base,
+    Strong
+} ;
+
+public delegate float CalculateModifier(float baseValue, Modifier modifier) ;
+
+class Player
+{
+    // access the Player’s properties directly from the main file.
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    public Player(string name= "Player", float maxHp= 100f)
+    {
+        this.name = name;
+        if (maxHp > 0)
+        {
+            this.maxHp = maxHp;
+        }
+        else
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        this.hp = this.maxHp;
+    }
+
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+        {
+            damage = 0 ;
+        }
+        Console.WriteLine($"{name} takes {damage} damage!");
+        ValidateHP(hp - damage);
+    }
+
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+        {
+            heal = 0 ;
+        }
+        Console.WriteLine($"{name} heals {heal} HP!");
+        ValidateHP(hp + heal);
+    }
+
+    public void ValidateHP(float newHp)
+    {
+        // If newHp is negative, set hp to 0
+        // If newHp is larger than maxHp, set hp to the value of maxHp
+        // Otherwise, set hp to the value of newHp
+        this.hp = newHp < 0 ? 0f : newHp > this.maxHp ? maxHp : newHp ;
+    }
+
+    public float ApplyModifier( float baseValue, Modifier modifier )
+    {        
+        return modifier == Modifier.Weak ? baseValue / 2 : modifier == Modifier.Strong ?  1.5f * baseValue : baseValue;
+    }
+
+}
