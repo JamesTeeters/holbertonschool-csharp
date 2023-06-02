2d7234811a024454891748c54c7dc198f7b2e8d3
JamesTeeters <4925@holbertonstudents.com>
2023-06-01 21:13:13 -0500

    create test object to test functionality

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public abstract class Base
+{
+    private string name {get; set;}
+
+    public override string ToString() => $"{this.name} is a {this.GetType()}";
+        
+}
+public interface IInteractive
+{
+    void Interact();
+}
+public interface IBreakable
+{
+    int durability {get; set;}
+    void Break();
+}
+public interface ICollectable
+{
+    bool isCollected {get; set;}
+    void Collect();
+}
+
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability {
+        get => throw new NotImplementedException();
+        set => throw new NotImplementedException();
+    }
+    public bool isCollected {
+        get => throw new NotImplementedException();
+        set => throw new NotImplementedException();
+    }
+    public string name {
+        get => throw new NotImplementedException();
+        set => throw new NotImplementedException();
+    }
+
+    public void Interact() {throw new NotImplementedException();}
+    public void Break() {throw new NotImplementedException();}
+    public void Collect() {throw new NotImplementedException();}
+}
