f3d2f514d400fd5bdd190341bc1bafa97da6c2d0
JamesTeeters <4925@holbertonstudents.com>
2023-06-01 22:56:29 -0500

    add damage delegation

---
+++ csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    public Player(string name= "Player", float maxHp= 100f)
+    {
+        this.name = name;
+        if (maxHp > 0)
+        {
+            this.maxHp = maxHp;
+        }
+        else
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        this.hp = this.maxHp;
+    }
+
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+
+    public delegate void  CalculateHealth(float property);
+
+    public void TakeDamage(float damage)
+    {
+        damage = (damage < 0) ? 0 : damage ;
+        Console.WriteLine($"{name} takes {damage} damage!");
+    }
+
+    public void HealDamage(float heal)
+    {
+        heal = (heal < 0) ? 0 : heal ;
+        Console.WriteLine($"{name} heals {heal} HP!");
+    }
+}
