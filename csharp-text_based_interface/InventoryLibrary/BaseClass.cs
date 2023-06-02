418bc0689dd540a286f2077ed79661efb7efc42b
JamesTeeters <4925@holbertonstudents.com>
2023-06-01 23:36:29 -0500

    Base Knowledge

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class BaseClass
+{
+    string id {get; set;};
+    DateTime date_created {get; set;};
+    DateTime date_updated {get; set;};
+
+    public BaseClass()
+    {
+        id = Guid.NewGuid().ToString();
+        date_created = DateTime.Now;
+        date_updated = date_created;
+    }
+
+    public string ToJSON()
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true,};
+        return JsonSerializer.Serialize<Object>(this, options);
+    }
+
+    public override string ToString() => $"[{this.GetType().Name}] ({this.id}) \n{ToJSON()}";
+}
