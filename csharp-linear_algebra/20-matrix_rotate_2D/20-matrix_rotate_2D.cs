b9da03331c94b51f3ffc87898e83546b66bac167
JamesTeeters <4925@holbertonstudents.com>
2023-06-01 19:05:28 -0500

    rotate a each value in a given matrix

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -7,32 +7,32 @@ class MatrixMath
     {
         double cos = Math.Cos(angle);
         double sin = Math.Sin(angle);
-        double[,] rotateMatrix = new double[,]{{cos, sin},{-sin, cos}};
+        double[,] rotateMatrix = new double[,] {
+            {cos, sin},
+            {-sin, cos}
+        };
 
         if (matrix is double[,] && matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
         {
-            int mRow1 = matrix.GetLength(0);
-            int mCol1 = matrix.GetLength(1);
-            int mRow2 = rotateMatrix.GetLength(0);
-            int mCol2 = rotateMatrix.GetLength(1);
+            int rowM1 = matrix.GetLength(0);
+            int colM1 = matrix.GetLength(1);
+            int colM2 = rotateMatrix.GetLength(1);
+            int rowM2 = rotateMatrix.GetLength(0);
 
-            double[,] matrixMul = new double[mRow1, mCol2];
+            double[,] mulMatrix = new double[rowM1, colM2];
 
-            for (int col = 0; col < mCol1; col++)
+            for (int col = 0; col < colM1; col++)
             {
-                for (int row = 0; row < mRow1; row++)
+                for (int row = 0; row < rowM1; row++)
                 {
-                    for (int i = 0; i < mCol2; i++)
+                    for (int ixj = 0; ixj < colM2; ixj++)
                     {
-                        matrixMul[row, i] = Math.Round(matrixMul[row, i] + matrix[row, col] * rotateMatrix[col, i], 2);
+                        mulMatrix[row, ixj] = Math.Round(mulMatrix[row, ixj] + matrix[row, col] * rotateMatrix[col, ixj], 2);
                     }
                 }
             }
-            return matrixMul;
+            return mulMatrix;
         }
-        else
-        {
-            return new double[,]{{-1}};
-        }
+        else  { return new double[,]{{-1}}; }
     }
 }
