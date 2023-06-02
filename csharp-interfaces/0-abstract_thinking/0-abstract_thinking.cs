d482a3717ab1da0c87ce14b6d508400f478cdd61
JamesTeeters <4925@holbertonstudents.com>
2023-06-01 20:42:03 -0500

    abstract Base class

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public abstract class Base
+{
+    public string name = null;
+
+    public override string ToString()
+    {
+        return name + "is a " + this.GetType();
+    }
+
+}
