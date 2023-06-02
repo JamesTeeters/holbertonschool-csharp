b9eca929f94b2fbdbaf051cd4e2e5c0cd65a799f
JamesTeeters <4925@holbertonstudents.com>
2023-06-01 21:23:22 -0500

    create a Door object that inherits from Base

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public abstract class Base
+{
+    public string name = null;
+
+    public override string ToString() => $"{this.name} is a {this.GetType()}";
+        
+}
+public interface IInteractive
+{
+    void Interact();
+}
+public interface IBreakable
+{
+    int durability {get; set;}
+    void Break();
+}
+public interface ICollectable
+{
+    bool isCollected {get; set;}
+    void Collect();
+}
+
+public class Door : Base, IInteractive
+{
+    public Door(string name = "Door"){
+        this.name = name;
+    }
+    public void Interact(){
+        Console.WriteLine($"You try to open the {this.name}. It's locked.");
+    }
+}
